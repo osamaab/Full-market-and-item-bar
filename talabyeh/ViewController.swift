e25cfbbc6b001efb739dd29088403cb568bd6a08
loai <loai.elayan@converged-technology.com>
2020-10-04 09:17:02 +0300

    Initial Commit

---
+++ talabyeh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  talabyeh
+//
+//  Created by loai elayan on 10/4/20.
+//  Copyright © 2020 Converged Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ talabyehTests/talabyehTests.swift
@@ -0,0 +1,34 @@
+//
+//  talabyehTests.swift
+//  talabyehTests
+//
+//  Created by loai elayan on 10/4/20.
+//  Copyright © 2020 Converged Technology. All rights reserved.
+//
+
+import XCTest
+@testable import talabyeh
+
+class talabyehTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
