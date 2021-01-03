a3b20ad742467b96bc69e9047f141eef3af576b9
Hussein Work <hus.sc@aol.com>
2021-01-03 15:44:49 +0200

    TAL-149

---
+++ talabyeh/Components/State Views/EmptyStateView.swift
@@ -0,0 +1,13 @@
+//
+//  EmptyStateView.swift
+//  talabyeh
+//
+//  Created by Hussein Work on 03/01/2021.
+//  Copyright © 2021 Dominate. All rights reserved.
+//
+
+import UIKit
+
+class EmptyStateView: GeneralStateView {
+}
+
